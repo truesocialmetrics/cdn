7a9cd7b19dda1f53e9b9c4b6967af8c4107985ad
Rostislav Mykhajliw <necromant2005@gmail.com>
2012-11-12 08:47:11 +0200

    make autoloading simpler

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,4 @@
 <?php
-$rootPath = realpath(dirname(__DIR__));
-$testsPath = "$rootPath/tests";
 
-if (is_readable($testsPath . '/TestConfiguration.php')) {
-    require_once $testsPath . '/TestConfiguration.php';
-} else {
-    require_once $testsPath . '/TestConfiguration.php.dist';
-}
-
-$path = array(
-    ZF2_PATH,
-    get_include_path(),
-);
-set_include_path(implode(PATH_SEPARATOR, $path));
-
+include_once __DIR__ . '/../vendor/autoload.php';
 include_once __DIR__ . '/../autoload_register.php';
