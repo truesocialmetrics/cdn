72e7d8fb348a326251c37821d1b6bfe16ea69d6e
Rostislav Mykhajliw <necromant2005@gmail.com>
2012-11-16 20:27:10 +0200

    update paths

--- bin/hash_collector.php
+++ bin/hash_collector.php
@@ -1,16 +1,10 @@
 #!/usr/bin/env php
 <?php
-if (__DIR__ . '/../autoload_register.php') {
-	include_once __DIR__ . '/../autoload_register.php';
-}
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-	include_once __DIR__ . '/../vendor/autoload.php';
-}
-if (file_exists(__DIR__ . '/../autoload.php')) {
-	include_once __DIR__ . '/../autoload.php';
+if (file_exists(__DIR__ . '/../../../../autoload.php')) {
+	include_once __DIR__ . '/../../../../autoload.php';
 }
 
-$collector = new TweeCdn\Hash\Collector(__DIR__ . '/../../../public');
+$collector = new TweeCdn\Hash\Collector(__DIR__ . '/../../../../public');
 $hashes = $collector->collect();
 
 $writer = new Zend\Config\Writer\PhpArray();
--- config/module.config.php
+++ config/module.config.php
@@ -1,39 +1,46 @@
 <?php
 return array(
-	'di' => array(
-		'definition' => array(
-			'TweeCdn\View\Helper\Cdn' => array(
-				'instantiator' => array(
-					'TweeCdn\View\Helper\CdnFactory',
-					'factory',
-				),
-			),
-		),
+    'di' => array(
+        'definition' => array(
+            'class' => array(
+                'TweeCdn\View\Helper\Cdn\AbstractCdn' => array(
+                    'instantiator' => array(
+                        'TweeCdn\View\Helper\CdnFactory',
+                        'factory',
+                    ),
+                ),
+                'TweeCdn\View\Helper\CdnFactory' => array(
+                    'factory' => array(
+                        'type' => array('type'=>'string', 'required' => true),
+                        'options' => array('type'=>'array', 'required' => true),
+                    ),
+                ),
+            ),
+        ),
         'instance' => array(
             'TweeCdn\\View\\Helper\\Cdn' => array(
                 'parameters' => array(
-                	// simple configuration
-                	'type'    => 'simple',
-                    'options' => array('public_dir' => __DIR__ . '/../../../public'),
+                    // simple configuration
+                    'type'    => 'simple',
+                    'options' => array('public_dir' => __DIR__ . '/../../../../public'),
 
                     // release configuration
-                	//'type'    => 'release',
+                    //'type'    => 'release',
                     //'options' => array(
-                    //    'public_dir' => __DIR__ . '/../../../public',
-                    //    // capistratno deploy revision
-                    //	  'release' => trim(file_get_contents(__DIR__ . '/../../../REVISION')),
+                    //    'public_dir' => __DIR__ . '/../../../../public',
+                    //    'release' => trim(file_get_contents(__DIR__ . '/../../../../REVISION')),
                     //),
 
                     // hash configuration
-                	//'type'    => 'hash',
+                    //'type'    => 'hash',
                     //'options' => array(
-                    //	'public_dir' => __DIR__ . '/../../../public',
-                    //	'hashes' => include __DIR__ . '/../tmp/hashes.php',
+                    //  'public_dir' => __DIR__ . '/../../../../public',
+                    //  'hashes' => (file_exists(__DIR__ . '/../tmp/hashes.php')) ? include __DIR__ . '/../tmp/hashes.php' : array(),
                     //),
                 ),
             ),
             'alias' => array(
-                'Cdn' => 'TweeCdn\\View\\Helper\\Cdn',
+                'cdn' => 'TweeCdn\View\Helper\Cdn\AbstractCdn',
             ),
         ),
     )
